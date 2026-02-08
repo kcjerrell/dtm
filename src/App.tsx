f3486198fae78536de51190a9c91e95675840c63
Kelly Jerrell <kcjerrell@gmail.com>
2026-02-08 02:28:58 -0800

    fix lazy loaded view components

--- src/App.tsx
+++ src/App.tsx
@@ -173,19 +173,13 @@ function ViewContainer(
     )
 }
 
-function viewLoader(path: string) {
-    return lazy(async () => {
-        return await import(path)
-    })
-}
-
 const views = {
-    metadata: viewLoader("./metadata/Metadata"),
-    mini: viewLoader("./Mini"),
-    vid: viewLoader("./vid/Vid"),
-    library: viewLoader("./library/Library"),
-    projects: viewLoader("./dtProjects/DTProjects"),
-    scratch: viewLoader("./scratch/Coffee"),
+    metadata: lazy(() => import("./metadata/Metadata")),
+    mini: lazy(() => import("./Mini")),
+    vid: lazy(() => import("./vid/Vid")),
+    library: lazy(() => import("./library/Library")),
+    projects: lazy(() => import("./dtProjects/DTProjects")),
+    // scratch: lazy(() => import("./scratch/Coffee")),
 }
 
 
