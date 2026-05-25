35e6f089d642cd8f8e80e47ff6de2c088876cc1a
Kelly Jerrell <kcjerrell@gmail.com>
2026-05-25 14:04:33 -0700

    remove motion from projects list

--- src/dtProjects/controlPane/projectsPanel/HiddenProjectsGroup.tsx
+++ src/dtProjects/controlPane/projectsPanel/HiddenProjectsGroup.tsx
@@ -1,4 +1,4 @@
-import { Collapsible, VStack } from "@chakra-ui/react"
+import { VStack } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { PanelListItem } from "@/components"
 import type { ProjectState } from "@/dtProjects/state/projects"
@@ -21,48 +21,35 @@ function HiddenProjectsGroup(props: HiddenProjectsGroupProps) {
     if (!projects?.length) return null
 
     return (
-        <Collapsible.Root open={showExcluded} onOpenChange={(e) => setShowExcluded(e.open)} asChild>
-            <VStack
-                justifyContent={"inherit"}
-                alignItems={"inherit"}
-                gap={"inherit"}
-                padding={0}
-                margin={0}
-                {...restProps}
+        <VStack
+            justifyContent={"inherit"}
+            alignItems={"inherit"}
+            gap={"inherit"}
+            padding={0}
+            margin={0}
+            {...restProps}
+        >
+            <PanelListItem
+                aria-label={showExcluded ? "Hide projects" : "Show hidden projects"}
+                onClick={() => setShowExcluded(!showExcluded)}
+                color="fg.3"
+                _hover={{ color: "fg.1" }}
+                alignSelf={"center"}
             >
-                <Collapsible.Trigger asChild>
-                    {/* <Collapsible.Indicator /> */}
-                    <PanelListItem
-                        aria-label={showExcluded ? "Hide projects" : "Show hidden projects"}
-                        onClick={() => setShowExcluded(!showExcluded)}
-                        color="fg.3"
-                        _hover={{ color: "fg.1" }}
-                        alignSelf={"center"}
-                    >
-                        {/* <HStack>
-                    <Box as={showExcluded ? FiRefreshCw : MdBlock} /> */}
-                        {/* <Box> */}
-                        {showExcluded
-                            ? "Hide projects"
-                            : `Show hidden projects (${projects.length})`}
-                        {/* </Box> */}
-                        {/* </HStack> */}
-                    </PanelListItem>
-                </Collapsible.Trigger>
-                <Collapsible.Content>
-                    {projects.map((p) => (
-                        <ProjectListItem
-                            marginLeft={2}
-                            key={p.path}
-                            project={p}
-                            onContextMenu={(e) => {
-                                onProjectContextMenu(e)
-                            }}
-                        />
-                    ))}
-                </Collapsible.Content>
-            </VStack>
-        </Collapsible.Root>
+                {showExcluded ? "Hide projects" : `Show hidden projects (${projects.length})`}
+            </PanelListItem>
+            {showExcluded &&
+                projects.map((p) => (
+                    <ProjectListItem
+                        marginLeft={2}
+                        key={p.path}
+                        project={p}
+                        onContextMenu={(e) => {
+                            onProjectContextMenu(e)
+                        }}
+                    />
+                ))}
+        </VStack>
     )
 }
 
