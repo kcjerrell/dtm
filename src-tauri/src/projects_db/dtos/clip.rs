a777ab125380925109833adec79c44c671450ac6
Kelly Jerrell <kcjerrell@gmail.com>
2026-06-29 00:16:13 -0700

    refactor: consolidate Clip model definition and implement FromRow for direct sqlx mapping

--- src-tauri/src/projects_db/dtos/clip.rs
+++ src-tauri/src/projects_db/dtos/clip.rs
@@ -1,41 +1,10 @@
 use serde::Serialize;
 use sqlx::{sqlite::SqliteRow, Row};
 
-use crate::projects_db::fbs::{root_as_clip, root_as_tensor_history_node};
-
-#[derive(Debug, Serialize, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct Clip {
-    pub row_id: i64,
-    pub clip_id: i64,
-    pub count: i32,
-    pub frames_per_second: f64,
-    pub width: i32,
-    pub height: i32,
-    pub audio_id: i64,
-}
-
-impl Clip {
-    pub fn map_row(row: &SqliteRow) -> Self {
-        let p = row.get::<Vec<u8>, _>("p");
-        let data = root_as_clip(&p).unwrap();
-        Self {
-            row_id: row.get("rowid"),
-            clip_id: row.get("__pk0"),
-            count: data.count(),
-            frames_per_second: data.frames_per_second(),
-            width: data.width(),
-            height: data.height(),
-            audio_id: data.audio_id(),
-        }
-    }
-}
-
-impl sqlx::FromRow<'_, SqliteRow> for Clip {
-    fn from_row(row: &SqliteRow) -> Result<Self, sqlx::Error> {
-        Ok(Self::map_row(row))
-    }
-}
+use crate::projects_db::{
+    dt_project::Clip,
+    fbs::{root_as_clip, root_as_tensor_history_node},
+};
 
 #[derive(serde::Serialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
